0c7ee1878c9cea6685f02f08da4ed7c479bc31ce
Tonnamkit <kittipat.bual@mail.kmutt.ac.th>
2023-04-25 14:39:04 +0700

    commit lab03

---
+++ src/LAB03.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class LAB03 {
+    public static void main(String[] args) {
+        try {
+            PrintWriter output = new PrintWriter("data.txt");
+            output.println("Hello World!");
+            output.println("Welcome to Java");
+            output.println("Harry Potter");
+            output.println(19);
+            output.println(60.5);
+            output.println(175.0);
+            output.close();
+        } catch (FileNotFoundException e) {
+            Logger.getLogger(LAB03.class.getName()).log(Level.SEVERE,null,e);
+        }
+
+        try {
+            FileReader file = new FileReader("data.txt");
+            BufferedReader input = new BufferedReader(file);
+            String s;
+            while ((s = input.readLine()) != null){
+                System.out.println(s);
+            }
+        } catch (FileNotFoundException e) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE,null,e);
+        } catch (IOException e) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE,null,e);
+        }
+    }
+}
