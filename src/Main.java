01e8b5329b3a2fadb8c2e0a4e9b310222d20d215
Armmy <tankm3310@gmail.com>
2023-04-25 14:27:19 +0700

    commit main

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,33 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.io.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
 public class Main {
     public static void main(String[] args) {
+        try {
+            FileWriter file = new FileWriter("data.txt");
+            BufferedWriter output = new BufferedWriter(file);
+            output.write("Hello World!");
+            output.write("Welcome to Java");
+            output.close();
+        } catch (IOException e) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE,null,e);
+        }
+
+        try {
+            FileReader file = new FileReader("data.txt");
+            BufferedReader input = new BufferedReader(file);
+            String s;
+            while ((s = input.readLine()) != null){
+                System.out.println(s);
+            }
+        } catch (FileNotFoundException e) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE,null,e);
+        } catch (IOException e) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE,null,e);
+        }
+
 
     }
 }
